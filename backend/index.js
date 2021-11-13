80fabd1cc3d5943cdc6ddd3281694cab1052a8e2
akashmadduru <akashmadduru@gmail.com>
2021-11-13 17:02:05 +0530

    updates

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,25 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-
 const port = process.env.PORT || 4000;
+const app = express();
 
 app.get("/", (req, res) => {
-  // var exec = require("child_process").exec;
-  // var coffeeProcess = exec("ab -c 50 -n 3000 api.canvasboard.live/");
-  // coffeeProcess.stdout.on("data", function (data) {
-  //   console.log(data);
-  // });
-  res.send("Welcome Akshay1111");
+  var exec = require("child_process").exec;
+  var coffeeProcess = exec("ab -c 50 -n 3000 api.canvasboard.live/");
+  coffeeProcess.stdout.on("data", function (data) {
+    console.log(data);
+  });
+  res.send("Welcome");
 });
-
-const app = express();
 
 const server = http.createServer(app);
 
 const io = socketIo(server);
 
-io.on("connection", (socket) => {
-  console.log("cool");
-});
+const x = [{}];
+
+io.on("connection", (socket) => {});
+io.emit("message", { data: x });
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
